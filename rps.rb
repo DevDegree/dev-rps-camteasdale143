96c850579c06ad2655c6fb578e572334c5a4293f
Cameron Teasdale <cameron.teasdale@shopify.com>
2019-09-10 11:25:41 -0400

    initial branch commit

--- rps.rb
+++ rps.rb
@@ -1,51 +1,63 @@
-rps_table = {
- Rock: 1,
-  Paper: 3,
-  Scissors: 5,
-}
+puts "Rock Paper Scissors"
 
-validInput = false;
+$player_score = [0,0]
+
+def playRPS()
+  validInput = false;
+
+  rps_table = {
+      Rock: 1,
+      Paper: 3,
+      Scissors: 5,
+  }
+    print "Player 1 - Enter your selection: "  
+    player_one_selection = rps_table[gets.chomp().intern()]
+
+    print "Player 2 - Enter your selection: "  
+    player_two_selection = rps_table[gets.chomp().intern()]
+
+    if ((player_one_selection.is_a? Integer) && (player_two_selection.is_a? Integer)) 
+      state = player_one_selection + player_two_selection
+      if (state == 4)
+        puts "Paper covers rock"
+        puts winningPlayer(player_one_selection, player_two_selection, 3)
+      elsif (state == 6)
+        puts "Rock crushes scissors"
+        puts winningPlayer(player_one_selection, player_two_selection, 1)
+      elsif (state == 8) 
+        puts "Scissors cuts paper"
+        puts winningPlayer(player_one_selection, player_two_selection, 5)
+      else
+        puts "Tie!"
+        playRPS()
+      end
+    else 
+      puts "No cheaters! Only Rock, Paper or Scissors are allowed."
+      playRPS()
+    end
+end
 
 def winningPlayer(player_one, player_two, winning_num)
+  match_sum = $player_score[0] + $player_score[1] + 1
   if (player_one == winning_num)
-    return "Player 1 Wins"
+    $player_score[0] += 1
+    return "Player 1 Wins Match #{match_sum}"
   elsif (player_two = winning_num)
-    return "Player 2 Wins"
+    $player_score[1] += 1
+    return "Player 2 Wins Match #{match_sum}"
   else
     return "something wrong has happened and it turns out I am actually a not good programmer after all"
   end
 end
 
-puts "Rock Paper Scissors"
 
-while !validInput
-  print "Player 1 - Enter your selection: "  
-  player_one_selection = rps_table[gets.chomp().intern()]
-
-  print "Player 2 - Enter your selection: "  
-  player_two_selection = rps_table[gets.chomp().intern()]
-
-  if ((player_one_selection.is_a? Integer) && (player_two_selection.is_a? Integer)) 
-    state = player_one_selection + player_two_selection
-    if (state == 4)
-      puts "Paper covers rock"
-      validInput = true;
-      puts winningPlayer(player_one_selection, player_two_selection, 3)
-    elsif (state == 6)
-      puts "Rock crushes scissors"
-      validInput = true;
-      puts winningPlayer(player_one_selection, player_two_selection, 1)
-    elsif (state == 8) 
-      puts "Scissors cuts paper"
-      validInput = true;
-      puts winningPlayer(player_one_selection, player_two_selection, 5)
-    else
-      puts "Tie!"
-    end
-  else 
-    puts "No cheaters! Only Rock, Paper or Scissors are allowed."
-  end
+while ($player_score[0] < 3 && $player_score[1] < 3)
+  playRPS()
 end
 
-
+if $player_score[0] == 3
+  puts "Player 1 wins game"
+else 
+  puts "Player 2 wins game"
+end
 
